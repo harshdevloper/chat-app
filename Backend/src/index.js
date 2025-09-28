5f45136ad9b264995622ca6a138d413d28f3bea1
harsh sharma <142721437+harshdevloper@users.noreply.github.com>
2025-09-28 08:57:08 +0530

    Update CORS origin for production deployment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-application-frontend-npjd.onrender.com",
     credentials: true,
   })
 );
