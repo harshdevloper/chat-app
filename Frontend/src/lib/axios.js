64fab991a0f82ed2e6788892f04af6b6ce791652
harsh sharma <142721437+harshdevloper@users.noreply.github.com>
2025-09-28 08:27:20 +0530

    Update axios baseURL for production environment

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://finance-tracker-backend-hxmj.onrender.com" : "/api",
   withCredentials: true,
 });
