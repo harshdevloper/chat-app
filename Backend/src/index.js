662a5b33ac3f8715dd85fe21e43fba13db940b2c
harsh sharma <142721437+harshdevloper@users.noreply.github.com>
2025-09-28 09:55:01 +0530

    Update CORS origin to include API path

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-application-frontend-npjd.onrender.com",
+    origin: "https://chat-application-frontend-npjd.onrender.com/api",
     credentials: true,
   })
 );
