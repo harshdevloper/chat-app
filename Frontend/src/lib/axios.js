057fdaf9be60084f4a5572a7f16c5b5ef9b5955b
harsh sharma <142721437+harshdevloper@users.noreply.github.com>
2025-09-28 08:49:25 +0530

    Fix axios baseURL for production environment

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://finance-tracker-backend-hxmj.onrender.com" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://finance-tracker-backend-hxmj.onrender.com",
   withCredentials: true,
 });
