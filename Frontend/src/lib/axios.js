3effb3d211f674658df337ba4da32eab5563cdcc
harsh sharma <142721437+harshdevloper@users.noreply.github.com>
2025-09-28 10:15:08 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development"
   ? "http://localhost:5001" // local dev backend
-  : "https://finance-tracker-backend-hxmj.onrender.com/api"; // deployed backend
+  : "https://finance-tracker-backend-hxmj.onrender.com"; // deployed backend
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
